abb3a9256c52e75cbef8a5e10a9b487f9f3eefa0
Parker Morse <flashesofpanic@gmail.com>
2012-09-17 14:47:10 -0400

    We don't need this for testing the dummy app.

--- spec/dummy/app/controllers/portal/offerings_controller.rb
+++ spec/dummy/app/controllers/portal/offerings_controller.rb
@@ -1,18 +1,8 @@
 class Portal::OfferingsController < ApplicationController
 
-  def setup_portal_student
-    learner = nil
-    if portal_student = current_user.portal_student
-      # create a learner for the user if one doesnt' exist
-      learner = @offering.find_or_create_learner(portal_student)
-    end
-    learner
-  end
-
   def answers
     @offering = Portal::Offering.find(params[:id])
     if @offering
-      learner = setup_portal_student
       # if learner && params[:questions]
       if params[:questions]
         # create saveables
@@ -20,7 +10,7 @@ class Portal::OfferingsController < ApplicationController
           # translate the dom id into an actual Embeddable
           embeddable = parse_embeddable(dom_id)
           # create saveable
-          create_saveable(embeddable, @offering, learner, value) if embeddable
+          create_saveable(embeddable, @offering, value) if embeddable
         end
       end
       flash[:notice] = "Your answers have been saved."
