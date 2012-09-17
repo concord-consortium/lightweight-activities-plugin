b09ab6185fc94b9478b9cc1ce1124ad84986489b
Parker Morse <flashesofpanic@gmail.com>
2012-09-17 09:41:41 -0400

    Setting up Lightweight Pages to appropriately handle Learners
    for [#35529669]

--- app/controllers/lightweight/interactive_page_controller.rb
+++ app/controllers/lightweight/interactive_page_controller.rb
@@ -18,10 +18,24 @@ module Lightweight
       else
         @offering = @activity.offerings.first
       end
+      if @offering
+        @learner = setup_portal_student
+      end
       all_pages = @activity.pages
       current_idx = all_pages.index(@page)
       @previous_page = (current_idx > 0) ? all_pages[current_idx-1] : nil
       @next_page = (current_idx < (all_pages.size-1)) ? all_pages[current_idx+1] : nil
+    end
+
+    private
+    # This is borrowed from the Portal::Offerings controller and should perhaps be more generalized.
+    def setup_portal_student
+      learner = nil
+      if defined? current_user and portal_student = current_user.portal_student
+        # create a learner for the user if one doesnt' exist
+        learner = @offering.find_or_create_learner(portal_student)
+      end
+      learner
     end
   end
 end
