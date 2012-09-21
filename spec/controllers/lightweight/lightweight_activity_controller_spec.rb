14c0eec7e83b62db84eb840afe56fd85de8a5920
Parker Morse <flashesofpanic@gmail.com>
2012-09-21 16:47:50 -0400

    Lots more setup than needed in this spec

--- spec/controllers/lightweight/lightweight_activity_controller_spec.rb
+++ spec/controllers/lightweight/lightweight_activity_controller_spec.rb
@@ -33,31 +33,6 @@ describe Lightweight::LightweightActivityController do
       # setup
       act = Lightweight::LightweightActivity.create!(:name => "Test activity")
       page = act.pages.create!(:name => "Page 1", :text => "This is the main activity text.")
-      interactive = Lightweight::MWInteractive.create!(:name => "MW model", :url => "http://google.com")
-      page.add_interactive(interactive)
-
-      # Add embeddables
-      or1 = Embeddable::OpenResponse.create!(:name => "Open Response 1", :prompt => "Why do you think this model is cool?")
-      or2 = Embeddable::OpenResponse.create!(:name => "Open Response 2", :prompt => "What would you add to it?")
-
-      mc1 = Embeddable::MultipleChoice.create!(:name => "Multiple choice 1", :prompt => "What color is chlorophyll?")
-      Embeddable::MultipleChoiceChoice.create(:choice => 'Red', :multiple_choice => mc1)
-      Embeddable::MultipleChoiceChoice.create(:choice => 'Green', :multiple_choice => mc1)
-      Embeddable::MultipleChoiceChoice.create(:choice => 'Blue', :multiple_choice => mc1)
-
-      mc2 = Embeddable::MultipleChoice.create!(:name => "Multiple choice 2", :prompt => "How many protons does Helium have?")
-      Embeddable::MultipleChoiceChoice.create(:choice => '1', :multiple_choice => mc2)
-      Embeddable::MultipleChoiceChoice.create(:choice => '2', :multiple_choice => mc2)
-      Embeddable::MultipleChoiceChoice.create(:choice => '4', :multiple_choice => mc2)
-      Embeddable::MultipleChoiceChoice.create(:choice => '7', :multiple_choice => mc2)
-
-      xhtml1 = Embeddable::Xhtml.create!(:name => "Xhtml 1", :content => "This is some <strong>xhtml</strong> content!")
-
-      page.add_embeddable(mc1)
-      page.add_embeddable(or1)
-      page.add_embeddable(xhtml1)
-      page.add_embeddable(or2)
-      page.add_embeddable(mc2)
 
       # get the rendering
       get :show, :id => act.id
