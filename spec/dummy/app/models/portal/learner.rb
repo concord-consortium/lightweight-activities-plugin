ccff5d15adcb565d0aad4b6f3dcfd5135c05a996
Parker Morse <flashesofpanic@gmail.com>
2012-09-26 10:38:34 -0400

    Handling the lack of real learners better in the dummy app

--- spec/dummy/app/models/portal/learner.rb
+++ spec/dummy/app/models/portal/learner.rb
@@ -1,6 +1,7 @@
 class Portal::Learner < ActiveRecord::Base
   self.table_name = :portal_learners
 
+  attr_accessible :offering
   belongs_to :offering, :class_name => "Portal::Offering", :foreign_key => "offering_id"
 
 end
