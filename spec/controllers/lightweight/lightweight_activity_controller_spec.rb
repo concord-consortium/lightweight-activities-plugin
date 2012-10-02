8e3f97c419e0fb59f30bed0d5103a147effd5790
Parker Morse <flashesofpanic@gmail.com>
2012-10-02 15:37:43 -0400

    Starting specs to describe LightweightActivity creation

--- spec/controllers/lightweight/lightweight_activity_controller_spec.rb
+++ spec/controllers/lightweight/lightweight_activity_controller_spec.rb
@@ -40,4 +40,39 @@ describe Lightweight::LightweightActivitiesController do
       response.should redirect_to activity_page_url(page)
     end
   end
+
+  describe 'index' do
+    context 'when the current user is not an author' do
+      it 'should redirect to the home page with an error message' do
+      end
+    end
+
+    context 'when the current user is an author' do
+      it 'should provide a link to create a new Lightweight Investigation on the index page' do
+        get :index
+        response.body.should match /<a[^>]+href='(\/lightweight\/activities\/)?new'[^>]>/
+      end
+
+      it 'should provide a list of authored Lightweight Investigations on the index page' do
+        pending "only needed for editing"
+        get :index
+        response.body.should match /<div[^>]+id="lightweight_activities_list">/
+      end
+    end
+  end
+
+  describe 'new' do
+    it 'should provide a form for naming and describing a Lightweight Investigation' do
+      pending "Finish index first"
+    end
+
+    it 'should create a new Lightweight Investigation when submitted with valid data' do
+      pending "Finish index first"
+    end
+
+    it 'should return to the form with an error message when submitted with invalid data' do
+      pending "Finish index first"
+    end
+  end
+
 end
