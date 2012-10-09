0aa261c1fd350cef46648dd85134cdff8d469111
Parker Morse <flashesofpanic@gmail.com>
2012-10-09 15:02:58 -0400

    Updating activity form with additional fields

--- spec/controllers/lightweight/lightweight_activity_controller_spec.rb
+++ spec/controllers/lightweight/lightweight_activity_controller_spec.rb
@@ -115,7 +115,8 @@ describe Lightweight::LightweightActivitiesController do
         response.body.should match /<form[^<]+action="\/lightweight\/activities\/#{act.id}"[^<]+method="post"[^<]*>/
         response.body.should match /<input[^<]+name="_method"[^<]+type="hidden"[^<]+value="put"[^<]+\/>/
         response.body.should match /<input[^<]+id="lightweight_activity_name"[^<]+name="lightweight_activity\[name\]"[^<]+type="text"[^<]+value="#{act.name}"[^<]*\/>/
-        response.body.should match /<textarea[^<]+id="lightweight_activity_description"[^<]+name="lightweight_activity\[description\]"[^<]*>[^<]*Activity to be edited[^<]*<\/textarea>/
+        response.body.should match /<textarea[^<]+id="lightweight_activity_description"[^<]+name="lightweight_activity\[description\]"[^<]*>[\s]*Activity to be edited[\s]*<\/textarea>/
+        response.body.should match /<textarea[^<]+id="lightweight_activity_related"[^<]+name="lightweight_activity\[related\]"[^<]*>[\s]*<\/textarea>/
       end
 
       it 'should include a link to add pages' do
