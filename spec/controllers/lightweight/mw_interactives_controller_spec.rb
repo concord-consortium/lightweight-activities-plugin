2563003f82b61a6d098cf776949462dbb3248328
Parker Morse <flashesofpanic@gmail.com>
2012-10-10 16:16:37 -0400

    Fixing specs namespaces

--- spec/controllers/lightweight/mw_interactives_controller_spec.rb
+++ spec/controllers/lightweight/mw_interactives_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe MwInteractivesController do
+describe Lightweight::MwInteractivesController do
 
 end
--- spec/helpers/lightweight/mw_interactives_helper_spec.rb
+++ spec/helpers/lightweight/mw_interactives_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe MwInteractivesHelper do
+describe Lightweight::MwInteractivesHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
