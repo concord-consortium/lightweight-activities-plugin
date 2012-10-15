3c734b3426fe36ed29f45dcce644feb6f4157b77
Parker Morse <flashesofpanic@gmail.com>
2012-10-15 16:12:54 -0400

    First stages of adding MwInteractives to pages

--- app/controllers/lightweight/mw_interactives_controller.rb
+++ app/controllers/lightweight/mw_interactives_controller.rb
@@ -2,5 +2,28 @@ require_dependency "lightweight/application_controller"
 
 module Lightweight
   class MwInteractivesController < ApplicationController
+    def new
+      create
+    end
+
+    def create
+      if (params[:page_id])
+        @page = Lightweight::InteractivePage.find(params[:page_id])
+        @interactive = Lightweight::MwInteractive.create!()
+        Lightweight::InteractiveItem.create!(:interactive_page => @page, :interactive => @interactive)
+        flash[:notice] = "Your new MW Interactive has been created."
+        redirect_to edit_page_mw_interactive_path(@page, @interactive)
+      else
+        @interactive = Lightweight::MwInteractive.create!()
+        flash[:notice] = "Your new MW Interactive has been created."
+        redirect_to edit_mw_interactive_path(@interactive)
+      end
+    end
+
+    def edit
+    end
+
+    def update
+    end
   end
 end
--- spec/controllers/lightweight/mw_interactives_controller_spec.rb
+++ spec/controllers/lightweight/mw_interactives_controller_spec.rb
@@ -1,24 +1,88 @@
 require 'spec_helper'
 
 describe Lightweight::MwInteractivesController do
+  render_views
+  before do
+    # work around bug in routing testing
+    @routes = Lightweight::Engine.routes
+  end
+
   describe 'index' do
+    it 'returns a list of available MW Interactives' do
+      pending 'Not working this yet'
+    end
   end
 
   describe 'show' do
+    it 'is not routable' do
+      begin
+        get :show, :id => 'foo'
+        throw 'should not have been able to route to show'
+      rescue
+      end
+    end
   end
 
-  describe 'new' do
+  context 'when the logged-in user is an ordinary user' do
+    describe 'interactives' do
+      it 'are not editable' do
+        pending 'we are not yet ready for this'
+      end
+    end
   end
 
-  describe 'create' do
-  end
+  context 'when the logged-in user is an author' do
+    context 'and an InteractivePage ID is provided' do
+      before do
+        @page = Lightweight::InteractivePage.create!()
+      end
 
-  describe 'edit' do
-  end
+      describe 'new' do
+        it 'automatically creates a new interactive' do
+          starting_count = Lightweight::MwInteractive.count()
+          join_count = Lightweight::InteractiveItem.count()
+          get :new, :page_id => @page.id
 
-  describe 'update' do
-  end
+          Lightweight::MwInteractive.count().should equal starting_count + 1
+          Lightweight::InteractiveItem.count().should equal join_count + 1
+        end
 
-  describe 'destroy' do
+        it 'redirects the submitter to the edit page' do
+          get :new
+          response.should redirect_to("/lightweight/mw_interactives/#{assigns(:interactive).id}/edit")
+        end
+      end
+
+      describe 'create' do
+        it 'creates an empty MW Interactive' do
+          post :create
+        end
+
+        it 'redirects the submitter to the edit page' do
+        end
+      end
+    end
+
+    describe 'edit' do
+      it 'shows a form with values of the MW Interactive filled in' do
+      end
+    end
+
+    describe 'update' do
+      it 'replaces the values of the MW Interactive to match submitted values' do
+      end
+
+      it 'returns to the edit page with a message indicating success' do
+      end
+
+      it 'returns to the edit page with an error on failure' do
+      end
+    end
+
+    describe 'destroy' do
+      it 'removes the requested MW Interactive from the database' do
+        pending 'deleting is not yet specified'
+      end
+    end
   end
 end
